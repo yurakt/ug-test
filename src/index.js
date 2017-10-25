594981d3b97e84f13074f1b7b684d043a47b2f3a
Yura Gavz <yuragavz@Yuras-MacBook-Pro.local>
2017-10-25 08:49:19 +0300

    Eject config and less loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './less/index.css'
+
+import './less/index.less'
 import App from './js/App'
+
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
